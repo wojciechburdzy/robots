b69c81599e070da281f5d5a17c1ed9a6288ad199
wojciechburdzy <wojciechburdzy@yahoo.co.uk>
2018-08-28 19:49:44 +0100

    WB: today only created robot and Grid

---
+++ src/main/java/Robot.java
@@ -0,0 +1,30 @@
+public class Robot {
+
+    private int x;
+    private int y;
+    private String orientation;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -5,10 +6,29 @@ import org.junit.Test;
 public class Tests {
 
     @Test
-    public void test(){
-        Robot robot = new Robot();
+    public void testGrid(){
 
         Grid grid = new Grid();
 
+        grid.setX(5);
+        grid.setY(3);
+
+        Assert.assertEquals(5,grid.getX());
+        Assert.assertEquals(3,grid.getY());
+    }
+
+    @Test
+    public void testRobot(){
+
+        Robot robot = new Robot();
+
+        robot.setX(5);
+        robot.setY(3);
+        robot.setOrientation("N");
+
+
+        Assert.assertEquals(5,robot.getX());
+        Assert.assertEquals(3,robot.getY());
+        Assert.assertEquals("N",robot.getOrientation());
     }
 }
